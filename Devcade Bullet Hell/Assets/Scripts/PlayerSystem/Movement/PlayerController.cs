8bbd6d8c123fbcd52e8e2c800cd9a1d614dba673
ActuallySanta <SantiBenitezFer@outlook.com>
2025-09-24 22:33:50 -0400

    Added comments to the player controller script

--- Devcade Bullet Hell/Assets/Scripts/PlayerSystem/Movement/PlayerController.cs
+++ Devcade Bullet Hell/Assets/Scripts/PlayerSystem/Movement/PlayerController.cs
@@ -2,34 +2,47 @@ using UnityEngine;
 using Rewired;
 using System.Collections;
 using System;
-using Unity.VisualScripting;
-using System.Xml;
 public class PlayerController : MonoBehaviour
 {
+    //The player class data used for this instance of the player
     public PlayerData data;
+
+    //Attached components for the player controller
     [SerializeField] Animator anim;
     [SerializeField] Rigidbody2D rb;
     [SerializeField] PlayerInputController input;
     [SerializeField] SpriteRenderer sprite;
 
+    //The color that the player started as, used for invincibility
     private Color originalColor;
 
+    //The attached state machine component responsible for state transitions
     private PlayerStateMachine stateMachine;
+
+    //A reference for the Rewired player, responsible for input
     private Player player;
 
+    //Event called when the player gets hurt
     public delegate void OnPlayerHurtEventHandler(object sender, float newHealthVal);
     public event OnPlayerHurtEventHandler onPlayerHurt;
 
+    //Event called when the player dies
     public delegate void OnPlayerDieEventHandler(int playerID);
     public event OnPlayerDieEventHandler onPlayerDie;
 
+    //Checks if the player can be damaged
     private bool isInvincible = false;
+
+    //How much health the player currently has
     public float currHealth;
+    
+    //The frequency of the sine function used for invincibility flashing
     [SerializeField] private float flashFreq = .05f;
 
     //Player Input
     public Vector2 inputVector { get; private set; }
 
+    //The 2 basic states that the player will use
     public Player_IdleState idleState { get; private set; }
     public Player_MoveState moveState { get; private set; }
 
@@ -72,6 +85,10 @@ public class PlayerController : MonoBehaviour
         stateMachine.CurrState.PhysicsUpdate();
     }
 
+    /// <summary>
+    /// Used by player states, sets the rigidbody's velocity
+    /// </summary>
+    /// <param name="_vel">The magnitude of the velocity vector</param>
     public void SetVelocity(float _vel)
     {
         rb.linearVelocity = inputVector * _vel;
